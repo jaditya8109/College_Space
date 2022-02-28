40bb42dcacc157f1cd7e75bd648dce50f1a9f58b
jaditya8109 <jaditya8109@gmail.com>
2022-02-28 21:02:02 +0530

    checkpoint3

--- Rest-API/index.js
+++ Rest-API/index.js
@@ -4,51 +4,51 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
-const multer = require("multer");
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/post");
-const router = express.Router();
-const path = require("path");
+const multer = require("multer");
+const path  = require("path");
 
 dotenv.config();
+ 
+// mongodb connection
+mongoose.connect(process.env.MONGO_URL, 
+    {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => console.log("Database connected!"))
+ .catch(err => console.log(err));   
 
-mongoose.connect(
-  process.env.MONGO_URL,
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  () => {
-    console.log("Connected to MongoDB");
-  }
-);
 app.use("/images", express.static(path.join(__dirname, "public/images")));
 
-//middleware
+// middlewares
+// body parser and parse in post req
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
 
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "public/images");
-  },
-  filename: (req, file, cb) => {
-    cb(null, req.body.name);
-  },
+    destination:(req, file, cb)=>{
+        cb(null, "public/images");
+    },
+    filename: (req, file, cb) => {
+        cb(null, req.body.name);
+    },
 });
 
-const upload = multer({ storage: storage });
-app.post("/api/upload", upload.single("file"), (req, res) => {
-  try {
-    return res.status(200).json("File uploded successfully");
-  } catch (error) {
-    console.error(error);
-  }
-});
+const upload = multer({storage: storage});
+app.post("/api/upload", upload.single("file"), (req,res)=>{
+    try{
+        return res.status(200).json("File uploaded successfully")
+    }catch(err){
+        console.log(err)
+    }
+})
 
-app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
 app.use("/api/post", postRoute);
 
-app.listen(8800, () => {
-  console.log("Backend server is running!");
-});
+app.listen(8800, ()=>{
+    console.log("Server running as it should ")
+})
+ 
